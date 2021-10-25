f94915df6ec8ff235a84ddc3ef70445ccf52b5fd
Justin Dela Cruz <Traxiliongames@gmail.com>
2021-10-24 21:36:25 -0400

    Enemies on the right side of the screen are moving

--- Assets/[Scripts]/EnemyController.cs
+++ Assets/[Scripts]/EnemyController.cs
@@ -13,9 +13,8 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    //changing this from horizontal to vertical in order to bounce off proper boundaries
-    public float verticalSpeed;
-    public float verticalBoundary;
+    public float horizontalSpeed;
+    public float horizontalBoundary;
     public float direction;
 
     // Update is called once per frame
@@ -27,19 +26,19 @@ public class EnemyController : MonoBehaviour
 
     private void _Move()
     {
-        transform.position += new Vector3(verticalSpeed * direction * Time.deltaTime, 0.0f, 0.0f);
+        transform.position += new Vector3( 0.0f, horizontalSpeed * direction * Time.deltaTime, 0.0f);
     }
 
     private void _CheckBounds()
     {
         // check right boundary
-        if (transform.position.y >= verticalBoundary)
+        if (transform.position.y >= horizontalBoundary)
         {
             direction = -1.0f;
         }
 
         // check left boundary
-        if (transform.position.y <= -verticalBoundary)
+        if (transform.position.y <= -horizontalBoundary)
         {
             direction = 1.0f;
         }
